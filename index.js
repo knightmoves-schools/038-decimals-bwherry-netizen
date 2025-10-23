eecbd5190c3a48ffa59b67a26d7e74fe8e0ed1d9
Brendon W. <bwherry@midland.k12.ia.us>
2025-10-23 12:19:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+var lengthInFeet = 1.5;
+
+var pricePerFoot = 2.50;
+
+var totalPrice = pricePerFoot * lengthInFeet;
+
+
+
+
+
+document.getElementById("result").innerHTML = totalPrice
+
+
+
+
+
+
 
